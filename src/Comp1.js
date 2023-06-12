f41bdc32105e8b97f46c63abfc37b0159939ade3
vijaytkd-VA <mohitevijaysinh8379@gmail.com>
2023-06-12 23:08:07 +0530

    js change

---
+++ src/Comp1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Comp(){
+
+return (
+    <>
+    <title>Title</title>
+    <h1>My First Page</h1>
+    <div>test</div></>
+
+)
+
+}
+
+export default Comp;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Comp from './Comp1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <Comp />
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
